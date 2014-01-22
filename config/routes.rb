a0d3fa4f8d4b7b29f8280941a091270d4c967fed
lramapathiran <lramapathiran@gmail.com>
2014-01-22 20:13:08 +0100

    landing foundation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 GardeEnf::Application.routes.draw do
+
+  root 'landing_page#landing'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
